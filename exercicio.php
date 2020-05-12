4379c977c3d40275bf175c2f5f260408fb0ca694
Joao Pedro <jp.ssilva@live.com>
2020-05-12 09:25:32 -0300

    Inclusão dos arquivos exercicio.css e exercicio.php

---
+++ exercicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="recursos\css\estilo.css">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@500;600&display=swap" rel="stylesheet">
+    <title>Exercícios</title>
+
+</head>
+
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercícios</h2>
+    </header>
+
+    <main class="principal">
+        <div class="conteudo">
+            
+        </div>
+        <!--Fim da div conteudo-->
+    </main>
+
+    <footer class="rodape">
+        COD3R & ALUNOS ₢ <?= date('Y');?>
+    </footer>
+</body>
+
+</html>
