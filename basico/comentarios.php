8cd7320d1501d5936060c6f3312134722ddeef9b
Joao Pedro <jp.ssilva@live.com>
2020-05-12 17:41:48 -0300

    Comentário e o Desafio

---
+++ basico/comentarios.php
@@ -0,0 +1,36 @@
+<div class="titulo">Comentários PHP</div>
+
+
+<?php
+echo ("Estou no PHP");
+//Meu comentario
+#Meu comentário
+?>
+
+<p>Meu primeiro parágrafo
+
+<!-- 
+Esse comentário vai para o navegador, basta fazer a inspeção
+
+Comentário HTML 01
+Comentário HTML 02
+Comentário HTML 03
+Comentário HTML 04
+
+-->
+
+
+
+
+</p>
+
+<?php 
+
+/*
+Linhas de cometarios
+
+*/
+
+?>
+
+<p>Meu segundo parágrafo</p>
---
+++ basico/desafio.php
@@ -0,0 +1,16 @@
+<div class="titulo"> Desafio PHP</div>
+
+<ul>
+
+<li>1 + 1 = <?= 2; ?> </li>
+
+<li> 
+    <?php 
+        echo '4 + 4 = 8 ' ;    
+    ?>
+</li>
+
+
+<li>8 + 8 = <?php print(8+8);?> </li>
+
+</ul>
