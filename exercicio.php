0bd0dc03931b2f5b37f9716d7deaab9d0ce71bd7
Joao Pedro <jp.ssilva@live.com>
2020-05-12 09:58:02 -0300

    Navageção #01

--- exercicio.php
+++ exercicio.php
@@ -5,22 +5,28 @@
     <meta charset="UTF-8">
 
     <link rel="stylesheet" href="recursos\css\estilo.css">
+    <link rel="stylesheet" href="recursos\css\exercicio.css">
     <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@500;600&display=swap" rel="stylesheet">
     <title>Exercícios</title>
 
 </head>
 
-<body>
+<body class = "exercicio">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Visualização do Exercícios</h2>
     </header>
 
+    <nav class="navegacao">
+            <a href="#" class="verde">Sem formatação</a>
+            <a href="index.php" class="vermelho">Voltar</a>
+        </nav><!--Fim da nav navegacao--> 
+
     <main class="principal">
         <div class="conteudo">
             
-        </div>
-        <!--Fim da div conteudo-->
+        </div> <!--Fim da div conteudo-->      
+        
     </main>
 
     <footer class="rodape">
