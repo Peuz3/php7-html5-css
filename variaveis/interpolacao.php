86cc5656a95af4d02c58679ae7ee3e6077dba7c3
Joao Pedro <jp.ssilva@live.com>
2020-05-22 10:53:47 -0300

    Interpolação

---
+++ variaveis/interpolacao.php
@@ -0,0 +1,20 @@
+<div class="titulo">Interpolação</div>
+
+<?php
+$numero = 10;
+echo $numero;
+//Diferença entre as aspas simples e duplas
+echo '<br> $numero';
+echo "<br> $numero";
+
+$texto = "A sua nota é: $numero";
+echo "<br> $texto";
+
+$objeto = "caneta";
+echo "<br> Eu tenho $numero {$objeto}s";
+echo "<br> Eu tinha 5 { $objeto}s, mas perdi 3 {$objeto }s";
+
+
+
+
+?>
